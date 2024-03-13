81bc6dc30bc897136cbf93c02630410983003515
maleesha <maleesharathnayake840@gmail.com>
2024-03-13 14:10:54 +0530

    python practical

---
+++ 2.py
@@ -0,0 +1,36 @@
+i=0
+while i<10:
+    print(i)
+    if(i==5):
+        break
+    i=i+1
+print()
+ 
+j=0
+while j<10:
+    j=j+1
+    if j==5:
+        continue
+    print(j)
+    
+print()
+
+for x in "university":
+    print(x)
+    
+print()
+
+for x in range(5):
+    print(x)
+
+print()
+
+for x in range(2,9):
+     print(x)
+
+print()
+
+for x in range(1,10,2):
+     print(x)
+
+print()
