4abf8e66b0e8f6d1eccc5d179ed9638e1c86bd7a
maleesha <maleesharathnayake840@gmail.com>
2024-03-13 14:37:21 +0530

    python practical

--- 2.py
+++ 2.py
@@ -1,36 +1,22 @@
-i=0
-while i<10:
-    print(i)
-    if(i==5):
-        break
-    i=i+1
-print()
- 
-j=0
-while j<10:
-    j=j+1
-    if j==5:
-        continue
-    print(j)
-    
-print()
-
-for x in "university":
-    print(x)
-    
-print()
-
-for x in range(5):
+list1=["apple","banana","cherry","orange"]
+print(list1)
+print(type(list1))
+for x in list1:
     print(x)
 
 print()
 
-for x in range(2,9):
-     print(x)
+list1[2]="watermelone"
+print(list1)
+
+print(len(list1))
+
+print(list1.pop())
+print(list1)
+list1.insert(1,"Strawberry")
+print(list1)
 
 print()
 
-for x in range(1,10,2):
-     print(x)
-
-print()
+tuple1=("car","van","bike")
+print(
